6d64bec1f3d9793ffb3e3fbc9a4852dca5bdaa29
jorditruji <jordimoreratrujillo@gmail.com>
2017-09-30 11:10:48 +0200

    tests

--- test_finetune.py
+++ test_finetune.py
@@ -13,16 +13,47 @@ import layers_builder as layers
 import utils
 import matplotlib.pyplot as plt
 import os
+from keras.layers import Activation, Dense, Flatten
 
 from pspnet import PSPNet50
 
 
 
 
-pspnet = PSPNet50(nb_classes=16, input_shape=(640, 480),
+pspnet_ini = PSPNet50(nb_classes=16, input_shape=(640, 480),
                               weights='pspnet50_ade20k')
+
+
+'''
+a=0
+for layer in pspnet_ini.model.layers:
+	print (layer)
+	layer.trainable = False
+	a=a+1
+print (a)
+
+
+
+inp = pspnet_ini.model.input                                           # input placeholder
+outputs = [layer.output for layer in pspnet_ini.model.layers]          # all layer outputs
+functors = [K.function([inp]+ [K.learning_phase()], [out]) for out in outputs]  # evaluation functions
+
+# Testing
+input_shape=(640,480,3)
+test = np.random.random(input_shape)[np.newaxis,...]
+layer_outs = [func([test, 1.]) for func in functors]
+print(layer_outs)
+
+
+last = pspnet_ini.model.output
+x = Flatten()(last)
+x = Dense(1024, activation='relu')(x)
+preds = Dense(16, activation='softmax')(x)
+
+model = Model(pspnet_ini.input, preds)
 
 pspnet.model.layers.pop()
 pspnet.model.outputs = [pspnet.model.layers[-1].output]
 pspnet.model.layers[-1].outbound_nodes = []
 pspnet.model.add(Dense(16, activation='softmax'))
+'''
