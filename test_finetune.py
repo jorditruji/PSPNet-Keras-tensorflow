8f9327e66a72c99da16911d6d9606575d6a4f1dd
jorditruji <jordimoreratrujillo@gmail.com>
2017-09-30 19:01:58 +0200

    finetune

--- test_finetune.py
+++ test_finetune.py
@@ -27,7 +27,7 @@ pspnet_ini = PSPNet50(nb_classes=150, input_shape=(640, 480),
 pspnet_ini.model.layers.pop()
 last_layer = pspnet_ini.model.layers[-1].output
 #new_layer = Flatten()(last_layer)
-new_layer =Dense(16, activation='softmax', name='my_dense')
+new_layer =Flatten()(Dense(16, activation='softmax', name='my_dense'))
 
 inp = pspnet_ini.model.input
 out = new_layer(pspnet_ini.model.layers[-1].output)
