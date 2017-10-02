0b3b7a5066ce7716981580ad3e84269bdac25400
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-02 18:53:45 +0200

    custom layer

--- test_finetune.py
+++ test_finetune.py
@@ -13,14 +13,15 @@ import layers_builder as layers
 import utils
 import matplotlib.pyplot as plt
 import os
-from keras.layers import Activation, Dense, Flatten, Conv2D, Lambda
+from keras.layers import Activation, Dense, Flatten, Conv2D, Lambda, Input
 from keras.models import Model,Sequential
 from pspnet import PSPNet50
 from data_load import *
 
+
 def resize_like(input_tensor, ref_tensor): # resizes input tensor wrt. ref_tensor
-    H, W = ref_tensor.get_shape()[1], ref.get_shape()[2]
-    return tf.image.resize_nearest_neighbor(inputs, [H.value, W.value])
+    H, W = ref_tensor.get_shape()[1], ref_tensor.get_shape()[2]
+    return tf.image.resize_nearest_neighbor(input_tensor, [H.value, W.value])
 
 
 
@@ -28,18 +29,31 @@ def resize_like(input_tensor, ref_tensor): # resizes input tensor wrt. ref_tenso
 pspnet_ini = PSPNet50(nb_classes=150, input_shape=(640, 480),
                               weights='pspnet50_ade20k')
 
-#pspnet_ini.model.summary(line_length=150)
+pspnet_ini.model.summary(line_length=150)
 pspnet_ini.model.layers.pop()
 layer_lambda = pspnet_ini.model.layers.pop()
-print (layer_lambda)
+pspnet_ini.model.layers.pop()
+print(layer_lambda.get_config())
+'''
+layer_lambda = pspnet_ini.model.layers.pop()
+layer_lambda.get_config()
+'''
 
+tf_resize = Input(shape=(640, 480))
 kernel_size=(1,1)
 new_layer=Conv2D(16, (1, 1), strides=(1, 1), padding='valid', data_format='channels_last', dilation_rate=(1, 1), activation='linear', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(pspnet_ini.model.layers[-1].output)
 #new_layer = Dense((640, 480), activation='relu', name='fc2')(new_layer)
-resized_tensor = Lambda(resize_like, arguments={'ref_tensor':(640, 480)})(new_layer)
+new_layer = Lambda(resize_like, arguments={'ref_tensor':tf_resize},name='custom')(new_layer)
+inp = pspnet_ini.model.input
 out =Dense(16, activation='softmax', name='my_dense')(new_layer)
+model2 = Model(inp, out)
+
+model2.summary(line_length=150)
 
 '''
+out =Dense(16, activation='softmax', name='my_dense')(new_layer)
+
+
 #{"name": "conv6", "config": {"filters": 16, "use_bias": true, "name": "conv6", "bias_regularizer": null, "strides": [1, 1], "data_format": "", "activation": "linear", "trainable": true, "kernel_constraint": null, "activity_regularizer": null, "padding": "valid", "bias_initializer": {"config": {}, "class_name": "Zeros"}, "kernel_initializer": {"config": {"seed": null, "distribution": "uniform", "scale": 1.0, "mode": "fan_avg"}, "class_name": "VarianceScaling"}, "bias_constraint": null, "kernel_size": [1, 1], "kernel_regularizer": null, "dilation_rate": [1, 1]}, "class_name": "Conv2D"
 last_layer = pspnet_ini.model.layers[-1].output
 #new_layer = Flatten()(last_layer)
@@ -50,12 +64,12 @@ out =Dense(16, activation='softmax', name='my_dense')(new_layer)
 
 
 #out = new_layer(pspnet_ini.model.layers[-1].output)
-'''
+
 inp = pspnet_ini.model.input
 model2 = Model(inp, out)
 
 #model2.summary(line_length=150)
-
+'''
 
 x_train, y_train = load_data('/imatge/jmorera/PSPNet-Keras-tensorflow/train.txt', 100)
 x_test, y_test = load_data('/imatge/jmorera/PSPNet-Keras-tensorflow/val.txt', 100)
@@ -87,7 +101,6 @@ model2.fit(x_train, y_train,
           verbose=1,
           validation_data=(x_test, y_test),
           )
-
 '''
 a=0
 for layer in pspnet_ini.model.layers:
@@ -120,4 +133,3 @@ pspnet.model.layers.pop()
 pspnet.model.outputs = [pspnet.model.layers[-1].output]
 pspnet.model.layers[-1].outbound_nodes = []
 pspnet.model.add(Dense(16, activation='softmax'))
-'''
