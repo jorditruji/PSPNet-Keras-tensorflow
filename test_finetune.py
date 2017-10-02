ddc37079641d38934932f30c11e2c311a26fb8f6
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-02 20:24:50 +0200

    custom layer

--- test_finetune.py
+++ test_finetune.py
@@ -80,7 +80,7 @@ model2 = Model(inp, out)
 '''
 
 
-model2.compile(loss="sparse_categorical_crossentropy", optimizer='sgd', metrics=['accuracy'])
+model2.compile(loss="categorical_crossentropy", optimizer='sgd', metrics=['accuracy'])
 model2.summary(line_length=150)
 x_train, y_train = load_data('/imatge/jmorera/PSPNet-Keras-tensorflow/train.txt', 100)
 x_test, y_test = load_data('/imatge/jmorera/PSPNet-Keras-tensorflow/val.txt', 100)
@@ -95,6 +95,7 @@ print (y_train.shape)
 print (y_test.shape)
 
 y_train = np_utils.to_categorical(y_train, 16)
+print (y_train)
 y_test = np_utils.to_categorical(y_test, 16)
 
 a=0
