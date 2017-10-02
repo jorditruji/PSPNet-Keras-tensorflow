e7aff5b72eef438f403564ffa8820af38e12a1f4
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-02 19:19:22 +0200

    custom layer

--- test_finetune.py
+++ test_finetune.py
@@ -80,7 +80,7 @@ x_train= np.squeeze(x_train)
 x_test = np.squeeze(x_test)
 
 
-#y_train = y_train.reshape(100, 307200)
+y_train = y_train.reshape(100, 307200)
 #y_test = y_test.reshape(-2, y_test.shape[-2])
 print (y_train.shape)
 print (y_test.shape)
