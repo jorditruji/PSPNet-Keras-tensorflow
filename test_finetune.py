9061dcb2f2f749bfe170e4dcd084cfff2bbac990
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-03 19:09:33 +0200

    custom layer

--- test_finetune.py
+++ test_finetune.py
@@ -20,7 +20,7 @@ def class_weighted_pixelwise_crossentropy(target, output):
      output = tf.clip_by_value(output, 10e-8, 1.-10e-8)
 
      #with open('class_weights.pickle', 'rb') as f:
-     weight = [0.1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ,2 ,2 ,2 ,2 ,2]
+     weight = [0.05, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1]
      return -tf.reduce_sum(target * weight * tf.log(output))
 
 # Force matplotlib to not use any Xwindows backend.
