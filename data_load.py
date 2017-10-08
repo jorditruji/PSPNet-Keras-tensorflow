0fd864fdf1df3a62b45cb02ccf995c034c9357ee
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-08 22:50:46 +0200

    prova

--- data_load.py
+++ data_load.py
@@ -172,7 +172,8 @@ def load_data_V2(path,num_img):
 				images.append(input_data)
 				a=prova[1]
 		#		print (a[:-4])
-				labels.append(np_utils.to_categorical(img2int(read_pgm(a[:-4],'>'))))
+				labels.append(img2int(read_pgm(a[:-4],'>')))
+
 				if i%1==0:
 					images=np.array(images)
 					labels=np.array(labels)
