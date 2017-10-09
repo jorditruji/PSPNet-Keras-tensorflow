3a8915f380722d3995312555164872668a404a95
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-09 20:00:45 +0200

    prova

--- fine_v5.py
+++ fine_v5.py
@@ -171,9 +171,9 @@ new_layer = Lambda(resize_like, arguments={'ref_tensor':tf_resize},name='custom'
 inp = pspnet_ini.model.input
 #out =Dense(256, activation='softmax', name='my_dense')(new_layer)
 #
-#out = K.argmax(new_layer.output,axis=-1)(new_layer)
+#
 out =Flatten()(new_layer)
-
+out = K.argmax(out.output,axis=-1)(out)
 #out=Lambda(depth_softmax, name='custom2')(new_layer)
 #out=Reshape((640*480, 16))(out)
 model2 = Model(inp, out)
