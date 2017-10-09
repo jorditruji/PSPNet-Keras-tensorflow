9f8d8d2d210156e14c46d8dded42a43e3dcdba49
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-09 19:51:30 +0200

    prova

--- data_load.py
+++ data_load.py
@@ -178,6 +178,7 @@ def load_data_V2(path,num_img):
 					images=np.array(images)
 					labels=np.array(labels)
 					images= np.squeeze(images)
+					labels = y_train.reshape(3, 307200)
 					#images= np.expand_dims(images,0)
 					yield images, labels
 					images = []
